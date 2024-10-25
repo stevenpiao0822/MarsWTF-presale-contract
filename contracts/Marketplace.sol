4032e3c8832c93ff8f02b010c400bcb3eb3355b3
dodger213 <mitsurukudo213@gmail.com>
2024-10-25 13:57:55 -0500

    [create] created marketplace contract

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {EnglishAuction} from "./auctions/EnglishAuction.sol";
+import {DutchAuction} from "./auctions/DutchAuction.sol";
+import {FixSale} from "./auctions/FixSale.sol";
+
+contract Marketplace {}
