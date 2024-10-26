26cf4911c0cded627caad4b84ca76bbd7538d520
dodger213 <mitsurukudo213@gmail.com>
2024-10-26 03:46:11 -0500

    [create] created marsWTF sol

---
+++ contracts/MarsWTF.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+/**
+ *  Submitted for verification at basescan.org on 2024-03-20
+ *
+ * 888b      888       888      88888888b       c8888b           888        88  8888888888  8888888888
+ *  8888    8888    88     88    88      88   88      88          88        88      88       88
+ *  88 88  88 88   88       88   88      88   88                  88        88      88       88   88
+ *  88  8888  88   88 88888 88   88 8888Y       Y8888b            88        88      88       8888888
+ *  88        88   88       88   88     88            88          88   88   88      88       88   88
+ *  88        88   88       88   88      88   88      88           88 8  8 88       88       88
+ * 888        88   88       88   88      88     Y8888Y               88  88         88       88
+ *
+ *
+ *                           Welcome to MarsWTF.
+ *
+ *  It's MarsWTF.  On Base.  And he's Based.  Like Super Based!
+ *  So... MarsWTF!
+ *  Excellent, huh?
+ *
+ *  MarsWTF is a cryptocurrency token on the, well, Base Chain.
+ *  It is led by a super based incubation team armed with super based ambitions on bringing the project to the highest heights of success.
+ *  It's focused on community building, stable growth through strategic promotional advancements and an open-ended ecosystem where each and every holder will have a voice in charting the course of the token.
+ *
+ *  As we've said, it's really super based!
+ *
+ *  We got the goods.  Let's go change the culture!
+ *  https://marscoin.wtf
+ **/
+pragma solidity ^0.8.19;
