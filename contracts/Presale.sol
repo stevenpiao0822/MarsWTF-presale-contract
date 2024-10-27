41d14ff8016e6af503e8be37f8f2d37f3767ea5e
dodger213 <mitsurukudo213@gmail.com>
2024-10-27 04:38:14 -0500

    [create] created presale sol

---
+++ contracts/Presale.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+// mainnet usdc address 0xdAC17F958D2ee523a2206206994597C13D831ec7
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
