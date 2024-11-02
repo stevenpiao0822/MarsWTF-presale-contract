6028e97b2f1d91a356f67ba2b0ac605c063c0462
dodger213 <mitsurukudo213@gmail.com>
2024-11-02 11:34:40 -0500

    [create[ created test script

---
+++ test/Test.ts
@@ -0,0 +1,25 @@
+import { ethers } from "hardhat";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import EnglishABI from './abi/englishAuction.json';
+import DutchABI from './abi/dutchAuction.json';
+import FixedABI from './abi/fixSale.json';
+
+let FeeToken:any;
+let FeeTokenAddress:any;
+let Collection:any;
+let CollectionAddress:any;
+let Marketplace:any;
+let MarketplaceAddress:any;
+let owner: any;
+let devTeam: any;
+let creator1: any;
+let creator2: any;
+let creator3: any;
+let buyer1: any;
+let buyer2: any;
+let buyer3: any;
+const percentForSeller: number = 85;
+const mintFee: string = "10";
+const marketFee: number = 0;
+const listFee: string = "0";
